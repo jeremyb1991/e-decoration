dd73a894e56e04e3de6b10a67c348dd9ace35ca4
b08092007 <jeremyb.git.1991@gmail.com>
2017-12-30 16:16:23 +0800

    sync

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 // Import Vue
 import Vue from 'vue'
 
@@ -10,10 +12,6 @@ import Framework7Vue from 'framework7-vue'
 // Import F7 iOS Theme Styles
 import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
 import Framework7ThemeColors from 'framework7/dist/css/framework7.ios.colors.min.css'
-/* OR for Material Theme:
-import Framework7Theme from 'framework7/dist/css/framework7.material.min.css'
-import Framework7ThemeColors from 'framework7/dist/css/framework7.material.colors.min.css'
-*/
 
 // Import App Custom Styles
 import AppStyles from './css/app.css'
@@ -44,42 +42,3 @@ new Vue({
     app: App
   }
 });
-
-
-/**
- * Created by xiaohe on 12/19/17.
- */
-//import Vue from 'vue'
-//import App from './App'
-//import VueRouter from 'vue-router'
-
-// Import F7
-//import Framework7 from 'framework7'
-
-
-//Vue.use(VueRouter)
-
-// 2. Define route components
-//import resources from './pages/resources.vue'
-//const Home = { template: resources }
-//const Foo = { template: '<div>foo</div>' }
-//const Bar = { template: '<div>bar</div>' }
-
-// 3. Create the router
-//const router = new VueRouter({
-//  mode: 'history',
-//  base: __dirname,
-//  routes: [
-//    { path: '/worker', component: resources },
-//    { path: '/foo', component: Foo },
-//    { path: '/bar', component: Bar }
-//  ]
-//})
-
-
-//new Vue({
-//  //el: '#App',
-//  router,
-//  render: h => h(App)
-//})
-
